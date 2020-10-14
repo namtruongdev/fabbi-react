0c4ea1187c4bd6df0a9bf2bd123e7cadc5b62de1
NAM TRUONG DUONG <namtruong@namtruong-pc>
2020-10-14 14:51:31 +0700

    Updates

---
+++ precache-manifest.9a59d3c67bc3ee3725487ee5e2e1504d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ad892eec34c5f1c8d4a915385b73de5",
+    "url": "/fabbi-react/index.html"
+  },
+  {
+    "revision": "5e6f90b6a89bef38a23d",
+    "url": "/fabbi-react/static/css/2.e10332d9.chunk.css"
+  },
+  {
+    "revision": "ece5a71a39aa2db8b8be",
+    "url": "/fabbi-react/static/css/main.25c1f009.chunk.css"
+  },
+  {
+    "revision": "5e6f90b6a89bef38a23d",
+    "url": "/fabbi-react/static/js/2.82bf19bf.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/fabbi-react/static/js/2.82bf19bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ece5a71a39aa2db8b8be",
+    "url": "/fabbi-react/static/js/main.22464a30.chunk.js"
+  },
+  {
+    "revision": "61812dce2fd1402046f2",
+    "url": "/fabbi-react/static/js/runtime-main.d7a62af0.js"
+  },
+  {
+    "revision": "51432290525856201ee983ac7c77e2cc",
+    "url": "/fabbi-react/static/media/background.51432290.jpg"
+  }
+]);
